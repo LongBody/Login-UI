15f11a8cecfa03ee3ed5228058601c0738904f80
LongBody <nguyenthanhlong1120@gmail.com>
2019-12-16 19:13:03 +0700

    facebook

--- views/Sign.js
+++ views/Sign.js
@@ -34,3 +34,23 @@ document.getElementById('signIn').addEventListener('click', function() {
 
     });
 })
+document.getElementById('sign').addEventListener('click', function() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        setScreen(logScreen)
+            // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+})
